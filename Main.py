aa67fe21e49a6354bd794a07824b6a3eb15edac3
Rivaldi22 <rivaldihidayatullah00@gmail.com>
2023-10-13 18:36:52 +0700

    first commit

---
+++ Main.py
@@ -0,0 +1,24 @@
+# variabel
+# menaruh/assignment nilai
+a = 10
+x = 6
+panjang = 1000
+
+# pemanggilan ke 1
+print("Nilai a = ", a)
+print("Nilai x = ", x)
+print("Nilai panjang = ", panjang)
+
+# penamaan
+nilai_y = 15 # harus menggunakan underscore
+juta10 = 10000000 # harus cantumkan angka dibelakang kalo mau
+nilaiZ = 17.5 # ini juga boleh
+
+# pemanggilan ke 2
+print("Nilai a = ", a)
+a = 7
+print("Nilai a = ", a)
+
+# assignment indirect
+b = a
+print("Nilai b = ", b)
